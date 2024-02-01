f3497a09b6884200fdcbc18205bcbc391f65050a
potzplitz <127513690+potzplitz@users.noreply.github.com>
2024-02-01 11:56:33 +0100

    added Thread.sleep(1000) as temp fix

--- EditGui/EditMe.cs
+++ EditGui/EditMe.cs
@@ -14,6 +14,9 @@ namespace osu_profiles.EditGui
             MainGUI gui = new MainGUI();
             GetMeData data = new GetMeData();
             Task.Run(() => data.getMe()).Wait();
+            
+            Thread.Sleep(1000); // temporärer fix, kommt weg wenn das mit dem Wait() funktioniert
+            
             Console.WriteLine("der string in editme:           " + data.GetMeDataString());
             
             var finishedToken = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(data.GetMeDataString());
